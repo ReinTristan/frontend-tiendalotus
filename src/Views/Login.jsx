e32fc94e42c52df85d3cf533af05e9bedb9e47d3
ReinTristan <zetaeugene@gmail.com>
2024-09-29 19:58:44 -0500

    login done

--- src/Views/Login.jsx
+++ src/Views/Login.jsx
@@ -1,138 +1,202 @@
-import React, { useState } from "react";
-import Image from "../assets/ImageLogin.jpg";
-
+import React, { useRef, useState } from 'react'
+import Image from '../assets/ImageLogin.jpg'
+import useAuthStore from '../stores/useAuthStore'
+import toast from 'react-hot-toast'
+import { useNavigate } from 'react-router-dom'
 export default function Login() {
-  const [isRegistering, setIsRegistering] = useState(false); // Estado para controlar el formulario mostrado
+	const [isRegistering, setIsRegistering] = useState(false) // Estado para controlar el formulario mostrado
+	const navigate = useNavigate()
+	const { login, register, users, isAuthenticated } = useAuthStore(
+		(state) => state
+	)
+	const emailRef = useRef()
+	const passwordRef = useRef()
+	const nameRegRef = useRef()
+	const emailRegRef = useRef()
+	const passwordRegRef = useRef()
+	const passwordConfirmRegRef = useRef()
+	console.log(users)
+	const sendFormRegister = (event) => {
+		event.preventDefault()
+		if (passwordRegRef.current.value !== passwordConfirmRegRef.current.value) {
+			toast.error('Las contraseñas no coinciden')
+			return
+		}
+		const name = nameRegRef.current.value
+		const email = emailRegRef.current.value
+		const password = passwordRegRef.current.value
+		register({ name, email, password })
+		toast.success('Usuario creado')
+		setIsRegistering(false)
+	}
+	const sendFormLogin = (event) => {
+		event.preventDefault()
+		const email = emailRef.current.value
+		const password = passwordRef.current.value
+		login({ email, password })
+		if (!isAuthenticated) {
+			toast.error('Usuario o contraseña incorrectos')
+			return
+		}
+		toast.success('Login exitoso')
+		navigate('/')
+	}
+	return (
+		<div className='pt-10 pb-16 flex items-center justify-evenly bg-black'>
+			<div className='w-1/4 -ml-10'>
+				<img
+					src={Image}
+					alt='Imagen redondeada'
+					className='h-full w-full object-cover rounded-2xl'
+				/>
+			</div>
 
-  return (
-    <div className="pt-10 pb-16 flex items-center justify-evenly bg-black">
-      <div className="w-1/4 -ml-10">
-        <img
-          src={Image}
-          alt="Imagen redondeada"
-          className="h-full w-full object-cover rounded-2xl"
-        />
-      </div>
+			{/* Contenedor principal con flexbox */}
+			<div className=' font-inria text-white mr-40 bg-transparent shadow-lg rounded-2xl flex overflow-hidden max-w-4xl justify-center'>
+				{/* Formulario de Login o Registro */}
+				<div className='p-12 flex flex-col justify-center w-full max-w-md'>
+					{' '}
+					{/* Ancho fijo para el contenedor */}
+					{isRegistering ? ( // Condicional para mostrar el formulario correspondiente
+						<>
+							<h2 className='text-3xl font-bold mb-6 text-center'>
+								Registrarse
+							</h2>
+							<form className='space-y-6' onSubmit={sendFormRegister}>
+								<div>
+									<label className='block text-2xl mb-1' htmlFor='name'>
+										Nombre
+									</label>
+									<input
+										type='text'
+										id='name'
+										ref={nameRegRef}
+										className='w-full p-3 rounded-2xl bg-stone-500 placeholder-white focus:outline-none focus:ring-2 focus:ring-orange-500'
+										placeholder='Tu nombre'
+									/>
+								</div>
 
-      {/* Contenedor principal con flexbox */}
-      <div className=" font-inria text-white mr-40 bg-transparent shadow-lg rounded-2xl flex overflow-hidden max-w-4xl justify-center">
-        {/* Formulario de Login o Registro */}
-        <div className="p-12 flex flex-col justify-center w-full max-w-md"> {/* Ancho fijo para el contenedor */}
-          {isRegistering ? ( // Condicional para mostrar el formulario correspondiente
-            <>
-              <h2 className="text-3xl font-bold mb-6 text-center">Registrarse</h2>
-              <form className="space-y-6">
-                <div>
-                  <label className="block text-2xl mb-1" htmlFor="name">
-                    Nombre
-                  </label>
-                  <input
-                    type="text"
-                    id="name"
-                    className="w-full p-3 rounded-2xl bg-stone-500 placeholder-white focus:outline-none focus:ring-2 focus:ring-orange-500"
-                    placeholder="Tu nombre"
-                  />
-                </div>
+								<div>
+									<label className='block text-2xl mb-1' htmlFor='new-email'>
+										Correo
+									</label>
+									<input
+										type='email'
+										id='new-email'
+										ref={emailRegRef}
+										className='w-full p-3 rounded-2xl bg-stone-500 placeholder-white focus:outline-none focus:ring-2 focus:ring-orange-500'
+										placeholder='correo@ejemplo.com'
+									/>
+								</div>
 
-                <div>
-                  <label className="block text-2xl mb-1" htmlFor="new-email">
-                    Correo
-                  </label>
-                  <input
-                    type="email"
-                    id="new-email"
-                    className="w-full p-3 rounded-2xl bg-stone-500 placeholder-white focus:outline-none focus:ring-2 focus:ring-orange-500"
-                    placeholder="correo@ejemplo.com"
-                  />
-                </div>
+								<div>
+									<label className='block text-2xl mb-1' htmlFor='new-password'>
+										Contraseña
+									</label>
+									<input
+										type='password'
+										id='new-password'
+										ref={passwordRegRef}
+										className='w-full p-3 rounded-2xl bg-stone-500 placeholder-white focus:outline-none focus:ring-2 focus:ring-orange-500'
+										placeholder='* * * * * * * *'
+									/>
+								</div>
 
-                <div>
-                  <label className="block text-2xl mb-1" htmlFor="new-password">
-                    Contraseña
-                  </label>
-                  <input
-                    type="password"
-                    id="new-password"
-                    className="w-full p-3 rounded-2xl bg-stone-500 placeholder-white focus:outline-none focus:ring-2 focus:ring-orange-500"
-                    placeholder="* * * * * * * *"
-                  />
-                </div>
+								<div>
+									<label
+										className='block text-2xl mb-1'
+										htmlFor='confirm-password'
+									>
+										Confirmar Contraseña
+									</label>
+									<input
+										type='password'
+										id='confirm-password'
+										ref={passwordConfirmRegRef}
+										className='w-full p-3 rounded-2xl bg-stone-500 placeholder-white focus:outline-none focus:ring-2 focus:ring-orange-500'
+										placeholder='* * * * * * * *'
+									/>
+								</div>
 
-                <div>
-                  <label className="block text-2xl mb-1" htmlFor="confirm-password">
-                    Confirmar Contraseña
-                  </label>
-                  <input
-                    type="password"
-                    id="confirm-password"
-                    className="w-full p-3 rounded-2xl bg-stone-500 placeholder-white focus:outline-none focus:ring-2 focus:ring-orange-500"
-                    placeholder="* * * * * * * *"
-                  />
-                </div>
+								<button
+									type='submit'
+									className='w-full py-3 px-4 mb-5 bg-orange-500 rounded-2xl text-black text-2xl hover:bg-white transition duration-200'
+								>
+									Registrarse
+								</button>
+							</form>
+							<div className='relative flex pt-5 mt-10 items-center flex-grow border-t border-stone-500'></div>
+							{/* Texto de inicio de sesión debajo del formulario */}
+							<div className='text-center'>
+								<p className='font-inria text-white'>
+									¿Ya tienes cuenta?
+									<a
+										onClick={() => setIsRegistering(false)}
+										className='text-blue-500 hover:underline cursor-pointer'
+									>
+										{' '}
+										Inicia sesión aquí
+									</a>
+								</p>
+							</div>
+						</>
+					) : (
+						<>
+							<h2 className='text-3xl font-bold mb-6 text-center'>
+								Iniciar Sesión
+							</h2>
+							<form className='space-y-6' onSubmit={sendFormLogin}>
+								<div>
+									<label className='block text-2xl mb-1' htmlFor='email'>
+										Correo
+									</label>
+									<input
+										type='email'
+										id='email'
+										ref={emailRef}
+										className='w-full p-3 rounded-2xl bg-stone-500 placeholder-white focus:outline-none focus:ring-2 focus:ring-orange-500'
+										placeholder='correo@ejemplo.com'
+									/>
+								</div>
 
-                <button
-                  type="submit"
-                  className="w-full py-3 px-4 mb-5 bg-orange-500 rounded-2xl text-black text-2xl hover:bg-white transition duration-200"
-                >
-                  Registrarse
-                </button>
-              </form>
-              <div className="relative flex pt-5 mt-10 items-center flex-grow border-t border-stone-500"></div>
-              {/* Texto de inicio de sesión debajo del formulario */}
-              <div className="text-center">
-                <p className="font-inria text-white">
-                  ¿Ya tienes cuenta? 
-                  <a onClick={() => setIsRegistering(false)} className="text-blue-500 hover:underline cursor-pointer"> Inicia sesión aquí</a>
-                </p>
-              </div>
-            </>
-          ) : (
-            <>
-              <h2 className="text-3xl font-bold mb-6 text-center">Iniciar Sesión</h2>
-              <form className="space-y-6">
-                <div>
-                  <label className="block text-2xl mb-1" htmlFor="email">
-                    Correo
-                  </label>
-                  <input
-                    type="email"
-                    id="email"
-                    className="w-full p-3 rounded-2xl bg-stone-500 placeholder-white focus:outline-none focus:ring-2 focus:ring-orange-500"
-                    placeholder="correo@ejemplo.com"
-                  />
-                </div>
+								<div>
+									<label className='block text-2xl mb-1' htmlFor='password'>
+										Contraseña
+									</label>
+									<input
+										type='password'
+										id='password'
+										ref={passwordRef}
+										className='w-full p-3 rounded-2xl bg-stone-500 placeholder-white  focus:outline-none focus:ring-2 focus:ring-orange-500'
+										placeholder='* * * * * * * *'
+									/>
+								</div>
 
-                <div>
-                  <label className="block text-2xl mb-1" htmlFor="password">
-                    Contraseña
-                  </label>
-                  <input
-                    type="password"
-                    id="password"
-                    className="w-full p-3 rounded-2xl bg-stone-500 placeholder-white  focus:outline-none focus:ring-2 focus:ring-orange-500"
-                    placeholder="* * * * * * * *"
-                  />
-                </div>
-
-                <button
-                  type="submit"
-                  className="w-full py-3 px-4 mb-5 bg-orange-500 rounded-2xl text-black text-2xl hover:bg-white transition duration-200"
-                >
-                  Iniciar Sesión
-                </button>
-              </form>
-              <div className="relative flex py-5 mt-10 items-center flex-grow border-t border-stone-500"></div>
-              {/* Texto de registro debajo del formulario */}
-              <div className="text-center">
-                <p className="font-inria text-white">
-                  No tienes cuenta? 
-                  <a onClick={() => setIsRegistering(true)} className="text-blue-500 hover:underline cursor-pointer"> Regístrate aquí</a>
-                </p>
-              </div>
-            </>
-          )}
-        </div>
-      </div>
-    </div>
-  );
+								<button
+									type='submit'
+									className='w-full py-3 px-4 mb-5 bg-orange-500 rounded-2xl text-black text-2xl hover:bg-white transition duration-200'
+								>
+									Iniciar Sesión
+								</button>
+							</form>
+							<div className='relative flex py-5 mt-10 items-center flex-grow border-t border-stone-500'></div>
+							{/* Texto de registro debajo del formulario */}
+							<div className='text-center'>
+								<p className='font-inria text-white'>
+									No tienes cuenta?
+									<a
+										onClick={() => setIsRegistering(true)}
+										className='text-blue-500 hover:underline cursor-pointer'
+									>
+										{' '}
+										Regístrate aquí
+									</a>
+								</p>
+							</div>
+						</>
+					)}
+				</div>
+			</div>
+		</div>
+	)
 }
--- src/stores/useAuthStore.js
+++ src/stores/useAuthStore.js
@@ -1,3 +1,4 @@
+import { nanoid } from 'nanoid'
 import { create } from 'zustand'
 import { persist } from 'zustand/middleware'
 
@@ -6,7 +7,6 @@ const defaultUsers = [
 	{
 		id: 1,
 		name: 'Admin User',
-		username: 'admin',
 		email: 'admin@example.com',
 		role: 'admin',
 		password: 'admin123', // En un caso real, nunca guardes contraseñas así
@@ -14,7 +14,6 @@ const defaultUsers = [
 	{
 		id: 2,
 		name: 'Regular User',
-		username: 'user',
 		email: 'user@example.com',
 		role: 'user',
 		password: 'user123',
@@ -22,48 +21,50 @@ const defaultUsers = [
 ]
 
 // Definimos la store de Zustand
-const useAuthStore = persist(
-	create((set, get) => ({
-		users: defaultUsers,
-		currentUser: null,
-		loginError: null,
+const useAuthStore = create(
+	persist(
+		(set, get) => ({
+			users: defaultUsers,
+			currentUser: null,
+			loginError: null,
 
-		// Función para manejar el login
-		login: (username, password) => {
-			set((state) => {
-				const user = state.users.find(
-					(u) => u.username === username && u.password === password
-				)
-				if (user) {
-					return { currentUser: user, loginError: null }
-				}
-				return { loginError: 'Invalid username or password' }
-			})
-		},
-		register: () => {
-			set((state) => {
-				const user = {
-					id: state.users.length + 1,
-					name: 'New User',
-					username: 'newuser',
-					email: '',
-					role: 'user',
-				}
-				return { users: [...state.users, user] }
-			})
-		},
+			// Función para manejar el login
+			login: ({ email, password }) => {
+				set((state) => {
+					const user = state.users.find(
+						(u) => u.email === email && u.password === password
+					)
+					if (user) {
+						return { currentUser: user, loginError: null }
+					}
+					return { loginError: 'Invalid username or password' }
+				})
+			},
+			register: ({ name, email, password }) => {
+				set((state) => {
+					const user = {
+						id: nanoid(),
+						name,
+						email,
+						password,
+						role: 'user',
+					}
+					return { users: [...state.users, user] }
+				})
+			},
 
-		// Función para desloguear
-		logout: () => {
-			set({ currentUser: null })
-		},
+			// Función para desloguear
+			logout: () => {
+				set({ currentUser: null })
+			},
 
-		// Función para verificar si hay un usuario logueado
-		isAuthenticated: () => {
-			return !!get().currentUser
-		},
-	})),
-	{ name: 'authStore' }
+			// Función para verificar si hay un usuario logueado
+			isAuthenticated: () => {
+				return !!get().currentUser
+			},
+		}),
+		{ name: 'authStore' }
+	)
 )
 
 export default useAuthStore
